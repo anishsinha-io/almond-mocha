46a421829bdeb8dbbfd88b3f552ec048fbdabd65
Anish Sinha <anishsinha0128@gmail.com>
2023-08-01 17:13:01 -0400

    Write skeleton for session middleware

--- src/app/auth/guards.rs
+++ src/app/auth/guards.rs
@@ -1,15 +1,20 @@
-use actix_web::dev::ServiceRequest;
 use actix_web::error::ErrorUnauthorized;
 use actix_web::HttpMessage;
+use actix_web::{dev::ServiceRequest, web::Data};
 use actix_web_httpauth::extractors::bearer::BearerAuth;
+
+use crate::app::state::AppState;
 
 use super::tokens::{self, Claims};
 
-// pub async fn session_guard(req: ServiceRequest) -> Result<ServiceRequest, (actix_web::error::Error, ServiceRequest)>{
-//     if let Some(session_state)= req.cookie("session_state") {
-//
-//     }
-// }
+pub async fn session_guard(
+    req: ServiceRequest,
+    state: Data<AppState>,
+) -> Result<ServiceRequest, (actix_web::error::Error, ServiceRequest)> {
+    // if let Some(session_state) = req.cookie("session_state") {}
+    println!("{}", state.name);
+    Ok(req)
+}
 
 pub async fn jwt_guard(
     req: ServiceRequest,
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{middleware::Logger, web, App, HttpServer};
-use app::{routes, state::AppState};
+use actix_web_httpauth::middleware::HttpAuthentication;
+use app::{guards, routes, state::AppState};
 
 mod app;
 
@@ -13,10 +14,11 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         // let auth = HttpAuthentication::bearer(guards::jwt_guard);
+        let session = HttpAuthentication::with_fn(guards::session_guard);
 
         App::new()
             .wrap(Logger::default())
-            // .wrap(auth)
+            .wrap(session)
             .app_data(state.clone())
             .service(web::scope("/api").configure(routes::config))
     })
