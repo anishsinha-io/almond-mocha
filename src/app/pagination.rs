39642fed5d2be63a8749827bc535e8e9570ae0fa
Anish Sinha <anishsinha0128@gmail.com>
2023-08-12 17:42:37 -0400

    Add test for pagination container in correct module

--- src/app/pagination.rs
+++ src/app/pagination.rs
@@ -15,3 +15,37 @@ impl<T> PaginationContainer<T> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::app::util;
+
+    use super::*;
+
+    #[test]
+    pub fn test_pagination() {
+        util::test_util::init();
+
+        let jennys_reading_list = vec![
+            ("The Westing Game", "Ellen Raskin"),
+            ("To Kill a Mockingbird", "Harper Lee"),
+            ("Lord of the Flies", "William Golding"),
+            ("Diary of Anne Frank", "Anne Frank"),
+            ("A Separate Peace", "John Knowles"),
+            ("The Hobbit", "J.R.R. Tolkien"),
+            ("The Age of Innocence", "Edith Wharton"),
+            ("Catcher in the Rye", "J.D. Salinger"),
+            ("A Raisin in the Sun", "Lorraine Hansberry"),
+            ("The Count of Monte Cristo", "Alexander Dumas"),
+        ];
+
+        let mut container = PaginationContainer::new(jennys_reading_list.clone(), 9);
+        assert!(!container.done);
+
+        container = PaginationContainer::new(jennys_reading_list.clone(), 10);
+        assert!(container.done);
+
+        container = PaginationContainer::new(jennys_reading_list.clone(), 11);
+        assert!(container.done);
+    }
+}
